6e4b1fadfabfb2056684d64645e4155a2d17684c
Antonio Carlos Flach <flach_br@hotmail.com>
2022-06-26 07:55:46 -0300

    User create tests

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,7 +1,21 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import BadRequestException from 'App/Exceptions/BadRequestException'
+import User from 'App/Models/User'
 
 export default class UsersController {
-  public async store({ response }: HttpContextContract) {
-    return response.created({})
+  public async store({ request, response }: HttpContextContract) {
+    const userPayload = request.only(['email', 'username', 'password', 'avatar'])
+    if (!userPayload.email || !userPayload.username || !userPayload.password) {
+      throw new BadRequestException('required data not provided', 422)
+    }
+
+    const userByEmail = await User.findBy('email', userPayload.email)
+    if (userByEmail) throw new BadRequestException('e-mail already in use', 409)
+
+    const userByUsername = await User.findBy('username', userPayload.username)
+    if (userByUsername) throw new BadRequestException('user name already in use', 409)
+
+    const user = await User.create(userPayload)
+    return response.created({ user })
   }
 }
--- tests/functional/users/users.spec.ts
+++ tests/functional/users/users.spec.ts
@@ -1,6 +1,13 @@
+import Database from '@ioc:Adonis/Lucid/Database'
 import { test } from '@japa/runner'
+import UserFactory from 'Database/factories/UserFactory'
 
-test.group('User', () => {
+test.group('User', (group) => {
+  group.each.setup(async () => {
+    await Database.beginGlobalTransaction()
+    await Database.rollbackGlobalTransaction()
+  })
+
   test('it should create an user', async ({ client, assert }) => {
     const userPayload = {
       email: 'test@test.com',
@@ -14,7 +21,58 @@ test.group('User', () => {
     assert.exists(response.body().user.id, 'Id undefined')
     assert.equal(response.body().user.email, userPayload.email)
     assert.equal(response.body().user.username, userPayload.username)
-    assert.equal(response.body().user.password, userPayload.password)
+    assert.notExists(response.body().user.password, 'Password defined')
     assert.equal(response.body().user.avatar, userPayload.avatar)
   })
+
+  test('it should return 409 when e-mail is already in use', async ({ client, assert }) => {
+    const { email } = await UserFactory.create()
+    const userPayload = {
+      email,
+      username: 'test',
+      password: 'test',
+      avatar: 'https://images.com/image/2',
+    }
+
+    const response = await client.post('/users').form(userPayload)
+    response.assertStatus(409)
+    assert.exists(response.body().message)
+    assert.exists(response.body().code)
+    assert.exists(response.body().status)
+    assert.equal(response.body().code, 'BAD_REQUEST')
+    assert.include(response.body().message, 'e-mail')
+    assert.equal(response.body().status, 409)
+  })
+
+  test('it should return 409 when user name is already in use', async ({ client, assert }) => {
+    const { username } = await UserFactory.create()
+    const userPayload = {
+      email: 'test@test.com',
+      username,
+      password: 'test',
+      avatar: 'https://images.com/image/2',
+    }
+
+    const response = await client.post('/users').form(userPayload)
+    response.dump()
+    response.assertStatus(409)
+    assert.exists(response.body().message)
+    assert.exists(response.body().code)
+    assert.exists(response.body().status)
+    assert.equal(response.body().code, 'BAD_REQUEST')
+    assert.include(response.body().message, 'user name')
+    assert.equal(response.body().status, 409)
+  })
+
+  test('it should return 422 when required data is not provided', async ({ client }) => {
+    const response = await client.post('/users').form({})
+    response.dump()
+    response.assertStatus(422)
+    // assert.exists(response.body().message)
+    // assert.exists(response.body().code)
+    // assert.exists(response.body().status)
+    // assert.equal(response.body().code, 'BAD_REQUEST')
+    // assert.include(response.body().message, 'user name')
+    // assert.equal(response.body().status, 409)
+  }).pin()
 })
